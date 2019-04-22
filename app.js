055309c0b72e638353b2ee98d0d5303909224c7c
Alyssa Wei <wei.alyssa@gmail.com>
2019-04-22 13:10:51 -0700

    Got to "Handling User Events" of mini-sprint

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+var GroceryList = (props) => {
+
+  var onListItemClick = (event) => {
+  console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.items[0]}</li>
+      <li>{props.items[1]}</li>
+    </ul>
+  );
+  };
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['Blueberries', 'Bananas']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
